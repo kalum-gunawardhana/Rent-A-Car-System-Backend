588333db501c3cbb5d352e493cdb6d3fd27a7d57
Kalum Gunawardhana <kguna415@gmail.com>
2025-03-04 20:10:16 +0530

    first commit

---
+++ src/main/java/edu/RentACarSystem/RentACarSystemApplication.java
@@ -0,0 +1,11 @@
+package edu.RentACarSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentACarSystemApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(RentACarSystemApplication.class, args);
+	}
+}
---
+++ src/test/java/edu/RentACarSystem/RentACarSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.RentACarSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentACarSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
